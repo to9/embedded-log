bc3d88388883d7c62a6bf2b9ce2a1bf10bc35c11
G.D <gaodongzi@126.com>
2023-11-22 13:27:44 +0800

    Update and rename readme.txt to test.c

---
+++ tests/test.c
@@ -0,0 +1,57 @@
+#include "log.h"
+
+char g_log_buff[512];
+
+void MID_LOG_Put(const char *str) {
+	printf("%s", str);
+}
+
+int main(int argc, char const* argv[]) {
+    log_init(g_log_buff, sizeof(g_log_buff), MID_LOG_Put);
+  
+    uint8_t buff[32] = "123456778abcdef2222";
+    printf("111111111111111111\r\n");
+    LOG_VBS("verbose message");
+    printf("111111111111111111\r\n");
+    LOG_VBS_HEX("verbose message hex", buff, 32);
+    printf("111111111111111111\r\n");
+    LOG_INF("info message");
+    printf("111111111111111111\r\n");
+    LOG_INF_HEX("info message hex\r\n", buff, 32);
+    LOG_DBG("debug message");
+    LOG_DBG_HEX("debug message hex", buff, 32);
+    LOG_WRN("warning message");
+    LOG_WRN_HEX("warning message hex", buff, 32);
+    LOG_ERR("error message");
+    LOG_ERR_HEX("error message hex", buff, 32);
+    LOG_ASS("assert message");
+    LOG_ASS_HEX("assert message hex", buff, 32);
+    log_set_level(LOG_LEVEL_WARNING);
+    LOG_VBS("verbose message");
+    LOG_VBS_HEX("verbose message hex", buff, 32);
+    LOG_INF("info message");
+    LOG_INF_HEX("info message hex", buff, 32);
+    LOG_DBG("debug message");
+    LOG_DBG_HEX("debug message hex", buff, 32);
+    LOG_WRN("warning message");
+    LOG_WRN_HEX("warning message hex", buff, 32);
+    LOG_ERR("error message");
+    LOG_ERR_HEX("error message hex", buff, 32);
+    LOG_ASS("assert message");
+    LOG_ASS_HEX("assert message hex", buff, 32);
+    log_set_level(LOG_LEVEL_ERROR);
+    LOG_VBS("verbose message");
+    LOG_VBS_HEX("verbose message hex", buff, 32);
+    LOG_INF("info message");
+    LOG_INF_HEX("info message hex", buff, 32);
+    LOG_DBG("debug message");
+    LOG_DBG_HEX("debug message hex", buff, 32);
+    LOG_WRN("warning message");
+    LOG_WRN_HEX("warning message hex", buff, 32);
+    LOG_ERR("error message");
+    LOG_ERR_HEX("error message hex", buff, 32);
+    LOG_ASS("assert message");
+    LOG_ASS_HEX("assert message hex", buff, 32);
+    printf("111111111111111111\r\n");
+    return 0;
+}
