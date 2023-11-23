7ee7a6de3df51f90ed35df5b6e2cf4aca05295fe
G.D <gaodongzi@126.com>
2023-11-23 13:52:17 +0800

    Update test.c
    updte test

--- tests/test.c
+++ tests/test.c
@@ -7,9 +7,11 @@ void MID_LOG_Put(const char *str) {
 }
 
 int main(int argc, char const* argv[]) {
+    uint8_t buff[32] = "123456778abcdef2222";
+    uint32_t sensors = 0u;
+	
     log_init(g_log_buff, sizeof(g_log_buff), MID_LOG_Put);
   
-    uint8_t buff[32] = "123456778abcdef2222";
     printf("111111111111111111\r\n");
     LOG_VBS("verbose message");
     printf("111111111111111111\r\n");
@@ -23,9 +25,10 @@ int main(int argc, char const* argv[]) {
     LOG_WRN("warning message");
     LOG_WRN_HEX("warning message hex", buff, 32);
     LOG_ERR("error message");
-    LOG_ERR_HEX("error message hex", buff, 32);
-    LOG_ASS("assert message");
-    LOG_ASS_HEX("assert message hex", buff, 32);
+    LOG_ERR_HEX("error message hex", buff, 32);    
+    LOG_ASS(sensors == 1);
+    LOG_ASS_MSG(sensors == 1, "assert message: %s, len: %d", buff, sizeof(buff));
+    LOG_ASS_HEX(sensors == 1, "assert message hex", buff, 32);
     log_set_level(LOG_LEVEL_WARNING);
     LOG_VBS("verbose message");
     LOG_VBS_HEX("verbose message hex", buff, 32);
@@ -37,8 +40,9 @@ int main(int argc, char const* argv[]) {
     LOG_WRN_HEX("warning message hex", buff, 32);
     LOG_ERR("error message");
     LOG_ERR_HEX("error message hex", buff, 32);
-    LOG_ASS("assert message");
-    LOG_ASS_HEX("assert message hex", buff, 32);
+    LOG_ASS(sensors == 1);
+    LOG_ASS_MSG(sensors == 1, "assert message: %s, len: %d", buff, sizeof(buff));
+    LOG_ASS_HEX(sensors == 1, "assert message hex", buff, 32);
     log_set_level(LOG_LEVEL_ERROR);
     LOG_VBS("verbose message");
     LOG_VBS_HEX("verbose message hex", buff, 32);
@@ -50,8 +54,9 @@ int main(int argc, char const* argv[]) {
     LOG_WRN_HEX("warning message hex", buff, 32);
     LOG_ERR("error message");
     LOG_ERR_HEX("error message hex", buff, 32);
-    LOG_ASS("assert message");
-    LOG_ASS_HEX("assert message hex", buff, 32);
+    LOG_ASS(sensors == 1);
+    LOG_ASS_MSG(sensors == 1, "assert message: %s, len: %d", buff, sizeof(buff));
+    LOG_ASS_HEX(sensors == 1, "assert message hex", buff, 32);
     printf("111111111111111111\r\n");
     return 0;
 }
